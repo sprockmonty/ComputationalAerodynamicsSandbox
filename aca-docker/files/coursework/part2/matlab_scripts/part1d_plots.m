9583ed689f2160c7635e01d5798faff7cc491125
Davey <nmd16@ic.ac.uk>
2020-01-29 19:25:13 +0000

    added large 3d sim files

--- aca-docker/files/coursework/part2/matlab_scripts/part1d_plots.m
+++ aca-docker/files/coursework/part2/matlab_scripts/part1d_plots.m
@@ -26,6 +26,28 @@ ylabel("C_L")
 grid on
 saveas(gcf,'plots/part2d_plots/CL_aoa','epsc')
 
+residual4 = readmatrix(['../data/part1d_data/residual4.csv']);
+residualNames = ["Continuity","X-momentum","Y-momentum","Energy","Tke","Sdr"];
+residualColour = ['r','b','m','k','g','c'];
 figure
+for i = 1:length(residualNames)
+    semilogy(residual4(:,1),residual4(:,i+1),'LineWidth',1.5,'color',residualColour(i))
+    hold on
+end
+legend(residualNames)
+title("Residuals")
+xlabel("Iteration")
+ylabel("Residual")
+saveas(gcf,'plots/part2d_plots/residuals4','epsc')
 
+figure
+ClCd4Conv = readmatrix(['../data/part1d_data/ClCd4.csv']);
+plot(ClCd4Conv(:,1),ClCd4Conv(:,2),'k','LineWidth',1.5)
+hold on
+plot(ClCd4Conv(:,1),ClCd4Conv(:,3),'r','LineWidth',1.5)
+title("Coefficent Convergence")
+xlabel("Iteration")
+ylabel("Coefficient Value")
+legend(["C_D","C_L"])
+saveas(gcf,'plots/part2d_plots/ClCdConv','epsc')
 
